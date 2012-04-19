27828842eb52c577ec1b258dd1e8d06ebdd4c40f
Volodymyr Sapsai <vsapsai@gmail.com>
2012-04-20 00:25:22 +0300

    Compiler. Initial version.

---
+++ mus/compiler.js
@@ -0,0 +1,45 @@
+var max = function(x, y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+};
+
+var compileToResult = function(musexpr, startTime, result) {
+    var endTime = startTime;
+    if ('note' === musexpr.tag) {
+        result.push({ tag: 'note', pitch: musexpr.pitch,
+                     start: startTime, dur: musexpr.dur });
+        endTime = endTime + musexpr.dur;
+    } else if ('seq' === musexpr.tag) {
+        endTime = compileToResult(musexpr.left, startTime, result);
+        endTime = compileToResult(musexpr.right, endTime, result);
+    } else if ('par' === musexpr.tag) {
+        var leftEndTime = compileToResult(musexpr.left, startTime, result);
+        var rightEndTime = compileToResult(musexpr.right, startTime, result);
+        endTime = max(leftEndTime, rightEndTime);
+    }
+    return endTime;
+};
+
+
+var compile = function(musexpr) {
+    var result = [];
+    compileToResult(musexpr, 0, result);
+    return result;
+};
+
+var melody_mus = 
+    { tag: 'seq',
+      left: 
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         right: { tag: 'note', pitch: 'b4', dur: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+console.log(melody_mus);
+console.log(compile(melody_mus));

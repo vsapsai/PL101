f7c313423b64d01af9db3b1c188e1104fb489e8a
Volodymyr Sapsai <vsapsai@gmail.com>
2012-04-29 15:31:08 +0300

    Added more known tests.

--- parser/parser.js
+++ parser/parser.js
@@ -4,9 +4,17 @@ var fs = require('fs'); // for loading files
 
 // Read file contents
 var data = fs.readFileSync('scheem.peg', 'utf-8');
+
 // Show the PEG grammar file
 console.log(data);
+
 // Create my parser
 var parse = PEG.buildParser(data).parse;
-// Do a test
+
+// Do tests
 assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] );
+// assert.equal( parse(""), undefined);
+assert.equal( parse("atom"), "atom" );
+assert.equal( parse("+"), "+" );
+assert.deepEqual( parse("(+ x 3)"), ["+", "x", "3"] );
+assert.deepEqual( parse("(+ 1 (f x 3 y))"), ["+", "1", ["f", "x", "3", "y"]] );

03d1057cc32c24d2664056af53cf5446e4a03a7e
Volodymyr Sapsai <vsapsai@gmail.com>
2012-04-29 16:29:15 +0300

    Variable number of spaces.

--- parser/parser.js
+++ parser/parser.js
@@ -18,5 +18,9 @@ assert.equal( parse("atom"), "atom" );
 assert.equal( parse("+"), "+" );
 assert.deepEqual( parse("(+ x 3)"), ["+", "x", "3"] );
 assert.deepEqual( parse("(+ 1 (f x 3 y))"), ["+", "1", ["f", "x", "3", "y"]] );
+// -- single expression in parenthesis
 assert.deepEqual( parse("(x)"), ["x"] );
 assert.deepEqual( parse("((x))"), [["x"]] );
+// -- variable number of spaces between subexpressions in expression
+assert.deepEqual( parse("(a  b)"), ["a", "b"] );
+assert.deepEqual( parse("(ab)"), ["ab"] );

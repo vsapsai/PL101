b7908765202fc77f84e7131f6074e503ca71fba4
Volodymyr Sapsai <vsapsai@gmail.com>
2012-04-29 16:57:58 +0300

    Added quote support.

--- parser/parser.js
+++ parser/parser.js
@@ -21,6 +21,7 @@ assert.deepEqual( parse("(+ 1 (f x 3 y))"), ["+", "1", ["f", "x", "3", "y"]] );
 // -- single expression in parentheses
 assert.deepEqual( parse("(x)"), ["x"] );
 assert.deepEqual( parse("((x))"), [["x"]] );
+
 // -- variable number of spaces between subexpressions in expression
 assert.deepEqual( parse("(a  b)"), ["a", "b"] );
 assert.deepEqual( parse("(ab)"), ["ab"] );
@@ -30,3 +31,9 @@ assert.deepEqual( parse(" ( a b  )"), ["a", "b"] );
 assert.deepEqual( parse("(a\tb)"), ["a", "b"] );
 assert.deepEqual( parse("(a\nb)"), ["a", "b"] );
 assert.deepEqual( parse(" (a \nb\t )\n"), ["a", "b"] );
+
+// -- quote support
+assert.deepEqual( parse("'x"), ["quote", "x"] );
+assert.deepEqual( parse("'(a b)"), ["quote", ["a", "b"]] );
+assert.deepEqual( parse(" ''x"), ["quote", ["quote", "x"]] ); // this is ridiculous but correct
+assert.deepEqual( parse("' (a b)"), ["quote", ["a", "b"]] ); // don't like space between ' and (, but not gonna fight with it

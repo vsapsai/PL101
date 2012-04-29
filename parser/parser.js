3b12c0a77abed60ad9cbcf152fd170a716fc9257
Volodymyr Sapsai <vsapsai@gmail.com>
2012-04-29 15:38:21 +0300

    It is OK if there is only one expression inside parenthesis.

--- parser/parser.js
+++ parser/parser.js
@@ -18,3 +18,5 @@ assert.equal( parse("atom"), "atom" );
 assert.equal( parse("+"), "+" );
 assert.deepEqual( parse("(+ x 3)"), ["+", "x", "3"] );
 assert.deepEqual( parse("(+ 1 (f x 3 y))"), ["+", "1", ["f", "x", "3", "y"]] );
+assert.deepEqual( parse("(x)"), ["x"] );
+assert.deepEqual( parse("((x))"), [["x"]] );

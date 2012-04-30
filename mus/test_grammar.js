56a43209e610cc9107785b193b5725353497625c
Volodymyr Sapsai <vsapsai@gmail.com>
2012-05-01 00:58:07 +0300

    Changed pitch/duration separator to colon.

--- mus/test_grammar.js
+++ mus/test_grammar.js
@@ -13,11 +13,11 @@ var parse = PEG.buildParser(data).parse;
 
 // Do tests
 // -- note
-assert.deepEqual(parse("a4 100", "note"),
+assert.deepEqual(parse("a4:100", "note"),
     {tag:"note", pitch:"a4", dur:100});
 
 // -- rest
-assert.deepEqual(parse("_ 200", "rest"),
+assert.deepEqual(parse("_:200", "rest"),
     {tag:"rest", dur:200});
 
 // -- seq
@@ -25,29 +25,29 @@ var expectedSequence =
     {tag:"seq",
     left:{tag:"note", pitch:"a4", dur:100},
     right:{tag:"note", pitch:"c5", dur:50}};
-assert.deepEqual(parse("a4 100;c5 50", "sequence"),
+assert.deepEqual(parse("a4:100;c5:50", "sequence"),
     expectedSequence);
-assert.deepEqual(parse("a4 100; c5 50", "sequence"),
+assert.deepEqual(parse("a4:100; c5:50", "sequence"),
     expectedSequence);
-assert.deepEqual(parse("a4 100 ; c5 50", "sequence"),
+assert.deepEqual(parse("a4:100 ; c5:50", "sequence"),
     expectedSequence);
-assert.deepEqual(parse("a4 100; c5 50; e4 250", "sequence"),
+assert.deepEqual(parse("a4:100; c5:50; e4:250", "sequence"),
     {tag:"seq",
     left: {tag:"note", pitch:"a4", dur:100},
     right: {tag:"seq",
         left: {tag:"note", pitch:"c5", dur:50},
         right: {tag:"note", pitch:"e4", dur:250}}});
-assert.deepEqual(parse("_ 100; a4 100", "sequence"),
+assert.deepEqual(parse("_:100; a4:100", "sequence"),
     {tag:"seq",
     left: {tag:"rest", dur:100},
     right: {tag:"note", pitch:"a4", dur:100}});
 
 // -- par
-assert.deepEqual(parse("a4 100, c5 50", "parallel"),
+assert.deepEqual(parse("a4:100, c5:50", "parallel"),
     {tag:"par",
     left:{tag:"note", pitch:"a4", dur:100},
     right:{tag:"note", pitch:"c5", dur:50}});
-assert.deepEqual(parse("a4 100, c5 50, e4 250", "parallel"),
+assert.deepEqual(parse("a4:100, c5:50, e4:250", "parallel"),
     {tag:"par",
     left: {tag:"note", pitch:"a4", dur:100},
     right: {tag:"par",
@@ -55,30 +55,30 @@ assert.deepEqual(parse("a4 100, c5 50, e4 250", "parallel"),
         right: {tag:"note", pitch:"e4", dur:250}}});
 
 // -- repeat
-assert.deepEqual(parse("repeat 3 a4 100", "repeat"),
+assert.deepEqual(parse("repeat:3 a4:100", "repeat"),
     {tag:"repeat", count:3,
     section: {tag:"note", pitch:"a4", dur:100}});
 
 // -- parentheses and mix of seq & par & repeat
-assert.deepEqual(parse("a4 100, c5 50; e4 250"),
+assert.deepEqual(parse("a4:100, c5:50; e4:250"),
     {tag:"par",
     left: {tag:"note", pitch:"a4", dur:100},
     right: {tag:"seq",
         left: {tag:"note", pitch:"c5", dur:50},
         right: {tag:"note", pitch:"e4", dur:250}}});
-assert.deepEqual(parse("(a4 100, c5 50); e4 250"),
+assert.deepEqual(parse("(a4:100, c5:50); e4:250"),
     {tag:"seq",
     left: {tag:"par",
         left: {tag:"note", pitch:"a4", dur:100},
         right: {tag:"note", pitch:"c5", dur:50}},
     right: {tag:"note", pitch:"e4", dur:250}});
 // add repeat to the mix
-assert.deepEqual(parse("repeat 3 a4 100; c5 50"),
+assert.deepEqual(parse("repeat:3 a4:100; c5:50"),
     {tag:"repeat", count:3,
     section: {tag:"seq",
              left: {tag:"note", pitch:"a4", dur:100},
              right: {tag:"note", pitch:"c5", dur:50}}});
-assert.deepEqual(parse("(repeat 3 a4 100); c5 50"),
+assert.deepEqual(parse("(repeat:3 a4:100); c5:50"),
     {tag:"seq",
     left: {tag:"repeat", count:3,
             section: {tag:"note", pitch:"a4", dur:100}},
@@ -86,8 +86,8 @@ assert.deepEqual(parse("(repeat 3 a4 100); c5 50"),
 
 // -- more parentheses flexibility
 var expectedNote = {tag:"note", pitch:"a4", dur:100};
-assert.deepEqual(parse("(a4 100)"), expectedNote);
-assert.deepEqual(parse(" ( a4 100\n)\t"), expectedNote);
+assert.deepEqual(parse("(a4:100)"), expectedNote);
+assert.deepEqual(parse(" ( a4:100\n)\t"), expectedNote);
 
 //TODO: allow trailing seq, par separators
 

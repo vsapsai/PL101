e0affa3853b0925fb9575d6862ecef0a0a9a98be
Volodymyr Sapsai <vsapsai@gmail.com>
2012-04-30 22:55:57 +0300

    Added support for 'sequence' and 'parallel'.

--- mus/test_grammar.js
+++ mus/test_grammar.js
@@ -12,9 +12,42 @@ var data = fs.readFileSync('mus.peg', 'utf-8');
 var parse = PEG.buildParser(data).parse;
 
 // Do tests
+// -- note
 assert.deepEqual(parse("a4 100", "note"),
     {tag:"note", pitch:"a4", dur:100});
+
+// -- rest
 assert.deepEqual(parse("_ 200", "rest"),
-	{tag:"rest", dur:200});
+    {tag:"rest", dur:200});
+
+// -- seq
+var expectedSequence =
+    {tag:"seq",
+    left:{tag:"note", pitch:"a4", dur:100},
+    right:{tag:"note", pitch:"c5", dur:50}};
+assert.deepEqual(parse("a4 100;c5 50", "sequence"),
+    expectedSequence);
+assert.deepEqual(parse("a4 100; c5 50", "sequence"),
+    expectedSequence);
+assert.deepEqual(parse("a4 100 ; c5 50", "sequence"),
+    expectedSequence);
+assert.deepEqual(parse("a4 100; c5 50; e4 250", "sequence"),
+    {tag:"seq",
+    left: {tag:"note", pitch:"a4", dur:100},
+    right: {tag:"seq",
+        left: {tag:"note", pitch:"c5", dur:50},
+        right: {tag:"note", pitch:"e4", dur:250}}});
+
+// -- par
+assert.deepEqual(parse("a4 100, c5 50", "parallel"),
+    {tag:"par",
+    left:{tag:"note", pitch:"a4", dur:100},
+    right:{tag:"note", pitch:"c5", dur:50}});
+assert.deepEqual(parse("a4 100, c5 50, e4 250", "parallel"),
+    {tag:"par",
+    left: {tag:"note", pitch:"a4", dur:100},
+    right: {tag:"par",
+        left: {tag:"note", pitch:"c5", dur:50},
+        right: {tag:"note", pitch:"e4", dur:250}}});
 
 console.log('tests passed');
